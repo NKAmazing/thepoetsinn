3dbc9f5978fbe64b70d3c10ea05a13e024ca7d0f
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-05-01 22:37:53 -0300

    Close #9 App reordenada

--- Backend/app.py
+++ Backend/app.py
@@ -1,17 +1,18 @@
+# Archivo principal de la aplicacion
+
 from main import create_app
 import os
 from main import db
 
-
+# Llamo a la funcion que crea la aplicacion
 app = create_app()
 
 
-
+# Activo el contexto de la aplicacion
 app.app_context().push()
 
 if __name__ == '__main__':
+    # Creo la base de datos
     db.create_all()
+    # Ejecuto la aplicacion con el modo debug activado
     app.run(debug = True, port = os.getenv("PORT"))
-
-
-# Comentario
